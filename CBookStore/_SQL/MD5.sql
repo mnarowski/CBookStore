3bc106c724ec031780e69b6bf4795e46969733c1
Marcin S. Narowski <axxxon.pl@gmail.com>
2013-02-01 16:10:52 +0100

    Clear 1 file

--- CBookStore/_SQL/MD5.sql
+++ CBookStore/_SQL/MD5.sql
@@ -5,11 +5,3 @@ CREATE FUNCTION [dbo].MD5(@data VARCHAR) RETURNS VARCHAR(32) as
 BEGIN 
 	RETURN LOWER(CONVERT(VARCHAR(32), HashBytes('MD5', @data), 2));
 END
-/**
-* Cena po zniżkach
-*/
-CREATE FUNCTION [dbo].PriceAfterDiscount(@book_id INTEGER) RETURNS REAL
-AS
-BEGIN
-
-END
