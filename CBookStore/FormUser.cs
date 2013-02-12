6cc045602b0e2e449a5b6dd6ea27d5fb05ac8717
Marcin S. Narowski <axxxon.pl@gmail.com>
2013-02-13 00:39:31 +0100

    Kolejna porcja zmian

--- CBookStore/FormUser.cs
+++ CBookStore/FormUser.cs
@@ -13,8 +13,10 @@ namespace CBookStore
     public partial class FormUser : Form
     {
         private SqlConnection conn= DBHelper.getConnection();
-        private SqlDataReader reader = null;
-
+        private DualNumerator dualNumerator;
+        private int max = 0;
+        private object[][] datas;
+        private string[] columns = { };
         public FormUser()
         {
             InitializeComponent();
@@ -55,17 +57,9 @@ namespace CBookStore
             initData();    
         }
 
-        public void initData() {
-            SqlCommand adapter = new SqlCommand("SELECT * FROM [dbo].[Użytkownicy]", conn);
-            reader = adapter.ExecuteReader();
-            initLoaded();
-        }
 
         public void initLoaded(){
-            if(reader.Read()){
-                this.textBox12.Text = reader.GetString(0);
-                this.textBox13.Text = reader.GetString(1);
-            }
+            
         }
 
 
@@ -82,6 +76,49 @@ namespace CBookStore
         private void button1_Click(object sender, EventArgs e)
         {
             //dodaj
+        }
+
+        public void initData()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Książki]", conn);
+            DataSet set = new DataSet();
+            set.Reset();
+            adapter.Fill(set);
+
+            DataTable datable = set.Tables["Table"];
+            int i = 0;
+            this.max = datable.Rows.Count;
+            DBHelper.Log(this.max.ToString());
+            datas = new object[max][];
+            foreach (DataRow dr in datable.Rows)
+            {
+                datas[i] = new object[6];
+                int v = 0;
+                foreach (string p in columns)
+                {
+                    datas[i][v] = dr[p];
+                    v++;
+                }
+                i++;
+            }
+
+            dualNumerator = new DualNumerator(datas, max);
+            initTexts(dualNumerator.GetFirst());
+
+        }
+
+        public void initTexts(object[] reader)
+        {
+            if (reader.Length == 0)
+            {
+                return;
+            }
+            //this.textBox1.Text = reader[1].ToString();
+            //this.textBox2.Text = reader[2].ToString();
+            //this.textBox3.Text = reader[3].ToString();
+            //this.textBox4.Text = Convert.ToString(Convert.ToDouble(reader[4]));
+            //this.textBox5.Text = reader[5].ToString();
+
         }
     }
 }
